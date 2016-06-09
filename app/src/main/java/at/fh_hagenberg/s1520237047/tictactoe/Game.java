e147aa3f00c8f8ca68a05160180d2cbcfea5ebd4
Gabriel Schützeneder <g.schuetzeneder@gmail.com>
2016-06-09 20:40:03 +0200

    Small adjustment

--- app/src/main/java/at/fh_hagenberg/s1520237047/tictactoe/Game.java
+++ app/src/main/java/at/fh_hagenberg/s1520237047/tictactoe/Game.java
@@ -1,7 +1,114 @@
 package at.fh_hagenberg.s1520237047.tictactoe;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 /**
  * Created by Gabriel on 09.06.16.
  */
-public class Game {
+public class Game extends Activity implements View.OnClickListener {
+
+    private GameBoard gameBoard;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+
+        ImageButton b = null;
+
+        b = (ImageButton) findViewById(R.id.row1_button1);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row1_button2);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row1_button3);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row2_button1);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row2_button2);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row2_button3);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row3_button1);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row3_button2);
+        b.setOnClickListener(this);
+        b = (ImageButton) findViewById(R.id.row3_button3);
+        b.setOnClickListener(this);
+
+        gameBoard = new GameBoard(this);
+    }
+
+    @Override
+    public void onClick(View _v) {
+        switch (_v.getId()) {
+            case R.id.row1_button1: {
+                if (gameBoard.startGame(0, 0))
+                    setPic(R.id.row1_button1);
+            }
+            break;
+            case R.id.row1_button2: {
+                if (gameBoard.startGame(0, 1))
+                    setPic(R.id.row1_button2);
+            }
+            break;
+            case R.id.row1_button3: {
+                if (gameBoard.startGame(0, 2))
+                    setPic(R.id.row1_button3);
+            }
+            break;
+            case R.id.row2_button1: {
+                if (gameBoard.startGame(1, 0))
+                    setPic(R.id.row2_button1);
+            }
+            break;
+            case R.id.row2_button2: {
+                if (gameBoard.startGame(1, 1))
+                    setPic(R.id.row2_button2);
+            }
+            break;
+            case R.id.row2_button3: {
+                if (gameBoard.startGame(1, 2))
+                    setPic(R.id.row2_button3);
+            }
+            break;
+            case R.id.row3_button1: {
+                if (gameBoard.startGame(2, 0))
+                    setPic(R.id.row3_button1);
+            }
+            break;
+            case R.id.row3_button2: {
+                if (gameBoard.startGame(2, 1))
+                    setPic(R.id.row3_button2);
+            }
+            break;
+            case R.id.row3_button3: {
+                if (gameBoard.startGame(2, 2))
+                    setPic(R.id.row3_button3);
+            }
+            break;
+        }
+    }
+
+    private void setPic(int ressource) {
+        ImageButton imageButton = (ImageButton) findViewById(ressource);
+        if (gameBoard.player == false) {
+            imageButton.setImageResource(R.drawable.cross);
+        } else
+            imageButton.setImageResource(R.drawable.circle);
+
+    }
+    public void reset() {
+        String string;
+        ImageButton imageButton;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                string = "row" + i + "_button" + j;
+                imageButton =  (ImageButton) findViewById(getResources().getIdentifier(string, "id", getPackageName()));
+                imageButton.setImageResource(R.drawable.blank);
+            }
+        }
+    }
 }
--- app/src/main/java/at/fh_hagenberg/s1520237047/tictactoe/MainActivity.java
+++ app/src/main/java/at/fh_hagenberg/s1520237047/tictactoe/MainActivity.java
@@ -1,13 +1,67 @@
 package at.fh_hagenberg.s1520237047.tictactoe;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    public static final String TAG = "Main";
+    public String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button button = null;
+
+        button = (Button) findViewById(R.id.button_enter);
+        button.setOnClickListener(this);
+        button = (Button) findViewById(R.id.button_connect_server);
+        button.setOnClickListener(this);
+        button = (Button) findViewById(R.id.button_start_server);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View _v) {
+        switch (_v.getId()) {
+            case R.id.button_enter: {
+                EditText editText = (EditText) findViewById(R.id.input_name);
+                if (editText.getTextSize() < 1)
+                    break;
+                else {
+                    name = editText.getText().toString();
+                    editText.clearFocus();
+                    //close keyboard
+                    InputMethodManager inputManager = (InputMethodManager) this.getSystemService(this.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                break;
+            }
+            case R.id.button_connect_server: {
+                //connect with server
+                //Test
+                Intent i = new Intent(this, Game.class);
+                this.startActivity(i);
+                break;
+            }
+            case R.id.button_start_server: {
+                //start connection
+                break;
+            }
+            default:
+                Log.e(TAG, "an unexpected ID encountered!");
+        }
     }
 }
